c6e2df3a8bb0e6d0e80eb2f76e2a925b920f0c41
Josh <jephillips@cohesionforce.com>
2015-09-05 17:20:47 -0500

    Added endpoint for testing

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.get('/', function(req, res) {
 
 app.use(express.static('app'));
 
+app.get('/notes', function(req, res) {
+    var note = {
+        title: 'Test note',
+        description: 'Test body'
+    };
+    res.json(note)
+});
+
 var server = app.listen(3000, function() {
     var host = server.address().address;
     var port = server.address().port;
