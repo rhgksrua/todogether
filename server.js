fff0529cd4664404e807dd90123c7a4a9c11aa22
Han Koh <rhgksrua@gmail.com>
2015-09-05 16:34:17 -0500

    serves static file

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.get('/', function(req, res) {
     res.send('Hello World');
 });
 
+app.use(express.static('app'));
+
 var server = app.listen(3000, function() {
     var host = server.address().address;
     var port = server.address().port;
