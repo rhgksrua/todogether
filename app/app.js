051d021391b0b61586d5174a1778276ee13d0414
Han Koh <rhgksrua@gmail.com>
2015-09-12 15:47:29 -0500

    user registration added

--- app/app.js
+++ app/app.js
@@ -1,26 +1,45 @@
-'use strict';
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ui.router','myApp.create','ngMessages'
-]).
-config(function($stateProvider, $urlRouterProvider) {
+        'ui.router', 
+        'myApp.create', 
+        'ngMessages'
+    ])
+    .config(['$httpProvider', '$stateProvider', '$urlRouterProvider', function($httpProvider, $stateProvider, $urlRouterProvider) {
         //
         // For any unmatched url, redirect to /state1
         $urlRouterProvider.otherwise("/main");
         //
         // Now set up the states
         $stateProvider
-            .state("base" , {
+            .state("base", {
                 url : "/main" ,
                 templateUrl : "partials/base.html"
             })
-            .state("login" ,{
-                url:"/login",
-                templateUrl:"partials/login.html" })
-            .state("register" ,{
-                url:"/newuser",
-                templateUrl:"partials/register.html"
+            .state("login", {
+                url: "/login",
+                templateUrl: "partials/login.html" 
             })
-        ;
-});
+            .state("register", {
+                url: "/register",
+                templateUrl: "partials/register.html",
+                controller: 'registerCtrl as rc'
+            });
+
+        // Every ajax request will atach authorization token in the request header if
+        // auth-token key in localStorage exists.
+        $httpProvider.interceptors.push('tokenInjector');
+    }])
+    .factory('tokenInjector', ['$window', function($window) {
+        // tokenInjector is injected into httpProvider
+        var tokenInjector = {
+            request: function(config) {
+                var token = $window.localStorage['auth-token'];
+                if (token) {
+                    config.headers['authorization'] = 'Bearer ' + token;
+                }
+                return config;
+            }
+        };
+        return tokenInjector;
+    }]);
--- app/partials/auth.register.controller.js
+++ app/partials/auth.register.controller.js
@@ -1,7 +1,112 @@
 /**
  * Created by pavan on 9/8/15.
+ *
+ * Implemented by hankhank on 9/12/15.
+ *
+ * auth.register.controller.js
+ *
+ * TODO: move services out from controller
+ *
  */
-var myApp=angular.module("myApp");
-myApp.controller("loginCtrl",function(){
+var myApp = angular.module('myApp');
 
-})
+myApp.controller('registerCtrl', ['registerService', '$location', function(registerService, $location) {
+    var rc = this;
+    rc.user = {};
+
+    /**
+     * register
+     *
+     * @param {object} user
+     * @return {undefined}
+     */
+    rc.register = function(user) {
+
+        // marker for displaying registration error
+        rc.serverError = false;
+        rc.emailExists = false;
+
+        registerService.register(user)
+            .then(function(response) {
+                if (response.data.error) {
+                    // server error
+                    rc.serverError = true;
+                    throw new Error('SERVER ERROR');
+                } else if (response.data.exists) {
+                    // email exists
+                    rc.emailExists = true;
+                    throw new Error('email exists');
+                }
+                return response;
+            })
+            .then(function(response) {
+                // Set token
+                registerService.setToken(response.data.token);
+                $location.path('/');
+                return response;
+            })
+            .then(null, function(response) {
+                // ajax fail
+                console.log('error', response);
+            });
+        };
+}])
+.service('registerService', ['$http', '$window', function($http, $window) {
+    // services could move out from the controller file
+
+    var reg = this;
+    var store = $window.localStorage;
+    var key = 'auth-token';
+    
+    /**
+     * setToken - set token received after registering
+     *
+     * @param {string} token
+     * @return {undefined}
+     */
+    reg.setToken = function(token) {
+        console.log('setting token');
+        if (token) {
+            store.setItem(key, token);
+        } else {
+            store.removeItem(key);
+        }
+    };
+
+    /**
+     * getToken - Get token from localStorage. Returns null if DNE.
+     *
+     * @return {undefined}
+     */
+    reg.getToken = function() {
+        return store.getItem(key);
+    };
+
+    /**
+     * sanitize - only allows email and password in user object
+     *
+     * @param {object} user
+     * @return {undefined}
+     */
+    var sanitize = function(user) {
+        var sanitizedUser = {
+            email: user.email,
+            password: user.password
+        };
+        return sanitizedUser;
+    };
+
+    /**
+     * register
+     *
+     * @param {object} user
+     * @return {undefined}
+     */
+    reg.register = function(user) {
+        user = sanitize(user);
+        return $http.post('/register', user)
+            .then(function(response) {
+                return response;
+            });
+    };
+}]);
